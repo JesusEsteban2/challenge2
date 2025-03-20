2b9c3d7fa3e7851671d36ee2cb3d940cd6e19c2a
Mananas <Mananas>
2025-03-20 13:21:39 +0100

    Update

--- src/prototipo/data/data.ts
+++ src/prototipo/data/data.ts
@@ -1,9 +1,39 @@
 export interface Product {
-    id: string,
-    name: string,
-    price: number,
-    stock: number,
-    is_active: boolean,
-    created_at: Date,
-    updated_at: Date,
+    id: string;
+    name: string;
+    price: number;
+    stock: number;
+    is_active: boolean;
+    created_at: Date;
+    updated_at: Date;
 }
+
+export const DATA = [
+    {
+        id: crypto.randomUUID() as string,
+        name: 'Producto1',
+        price: 1,
+        stock: 100,
+        is_active: true,
+        created_at: new Date(),
+        updated_at: new Date(),
+    },
+    {
+        id: crypto.randomUUID,
+        name: 'Producto2',
+        price: 2,
+        stock: 100,
+        is_active: true,
+        created_at: new Date(),
+        updated_at: new Date(),
+    },
+    {
+        id: crypto.randomUUID,
+        name: 'Producto3',
+        price: 3,
+        stock: 100,
+        is_active: true,
+        created_at: new Date(),
+        updated_at: new Date(),
+    },
+];
--- src/prototipo/index.ts
+++ src/prototipo/index.ts
@@ -1,22 +1,172 @@
 import { createServer } from 'node:http';
-import { createApp } from './app.js';
+import express from 'express';
+import { resolve } from 'path';
+import cors from 'cors';
+import { Router } from 'express';
+import type { NextFunction, Request, Response } from 'express';
+import { HttpError } from './http.error.type';
+import { DATA } from './data/data.js';
 
-// Puerto de escucha del servidor.
-const PORT = 3000;
+//Datos de prueba
+let dataArray = DATA;
 
-// Creación del servidor
-try {
-    // Inicializa Express
-    const server = createServer(createApp());
-    server.listen(PORT);
-    //CallBack de inicialización de servidor.
-    server.on('listening', () => listenManager());
-} catch (err) {
-    console.error('Server Error', err);
-    process.exit(1);
+// Error 404 - Not Found
+function notFoundController(req: Request, _res: Response, next: NextFunction) {
+    console.log('Petición recibida');
+    const message = `Page ${req.url} not found`;
+    const error = new HttpError(message, 404, 'Not Found');
+    next(error);
 }
 
-// Imprime mensaje de escucha del servidor, al resolver el callback.
-const listenManager = () => {
-    console.log(`Server listening on http:/localhost:${PORT}`);
+// Error 405 - Method Not Allowed
+function notMethodController(req: Request, _res: Response, next: NextFunction) {
+    console.log('Petición recibida');
+    const message = `Method ${req.method}  not allowed`;
+    const error = new HttpError(message, 405, 'Method Not Allowed');
+    next(error);
+}
+
+// Error 500 - Internal Server Error
+function errorController(
+    error: Error | HttpError,
+    _req: Request,
+    response: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction,
+) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+    // Errores de Prisma
+    // if (error instanceof PrismaClientKnownRequestError) {
+    //     const err = new HttpError(error.message, 400, 'Bad request error.');
+    //     error = err;
+    // }
+
+    if (!('status' in error)) {
+        error = {
+            ...error,
+            statusCode: 500,
+            status: 'Internal Server Error',
+        };
+    }
+
+    const publicMessage = `Error: ${error.statusCode} ${error.status}`;
+    console.log(publicMessage, error.message);
+
+    const html = `<p>${publicMessage}</p>`;
+    response.statusCode = error.statusCode;
+    response.statusMessage = error.status;
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    response.end(html);
+}
+
+// Product Get All
+function productGetAll(req: Request, res: Response) {
+    console.log('getAll');
+    res.json(dataArray);
+}
+
+// product Get By Id
+function productGetById(req: Request, res: Response) {
+    console.log('getById');
+    const { id } = req.params;
+    const product = dataArray.find((pro) => pro.id === id);
+    res.json(product);
+}
+
+function productPost(req: Request, res: Response) {
+    console.log('Post');
+    const pro = req.body;
+    dataArray.push(pro);
+    res.json(dataArray);
+}
+
+function productPatch(req: Request, res: Response) {
+    console.log('Patch');
+    const prodChang = req.body;
+    let product = dataArray.find((pro) => pro.id === prodChang.id);
+    product = { ...prodChang };
+    console.log(product);
+    res.json(dataArray);
+}
+
+function productDelete(req: Request, res: Response) {
+    const { id } = req.params;
+    const newData = dataArray.filter((pro) => pro.id !== id);
+    dataArray = newData;
+    res.json(dataArray);
+}
+
+function createProductRouter() {
+    console.log('Creating Product Router');
+    const productRouter = Router();
+
+    // getAll
+    productRouter.get('/', productGetAll);
+    // getById
+    productRouter.get('/:id', productGetById);
+    // add film
+    productRouter.post('/', productPost);
+    // modify by id
+    productRouter.patch('/:id', productPatch);
+    //delete
+    productRouter.delete('/:id', productDelete);
+
+    return productRouter;
+}
+
+const createApp = () => {
+    console.log('Iniciando App...');
+
+    // Inicialización de express
+    const app = express();
+
+    // Tomar path de la carpeta public.
+    const publicPath = resolve('public');
+
+    // Registrando Middleware.
+    console.log('Registering Middleware...');
+    // Habilitar Cors entre servidores.
+    app.use(cors());
+    // Usar parseado de Json.
+    app.use(express.json());
+
+    // Establecer ruta estática del proyecto desde public
+    app.use(express.static(publicPath));
+
+    /* App Routes */
+    console.log('Creating routes...');
+
+    // Registrar
+    app.use('/productos', createProductRouter());
+
+    // Not found & not method
+    app.get('*', notFoundController);
+    app.use('*', notMethodController);
+
+    app.use(errorController);
+
+    return app;
 };
+
+{
+    // Modulo principal de creación de servidor.
+    const PORT = 3000;
+
+    // Creación del servidor
+    try {
+        // Inicializa Express
+        const server = createServer(createApp());
+        server.listen(PORT);
+        //CallBack de inicialización de servidor.
+        server.on('listening', () => listenManager());
+    } catch (err) {
+        console.error('Server Error', err);
+        process.exit(1);
+    }
+
+    // Imprime mensaje de escucha del servidor, al resolver el callback.
+    const listenManager = () => {
+        console.log(`Server listening on http:/localhost:${PORT}`);
+    };
+}
